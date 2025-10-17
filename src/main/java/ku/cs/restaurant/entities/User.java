64db2f399485d3fea8de2f49f0e49a1ba4530bc4
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-10-17 11:11:57 +0700

    add Springboot security to pom.xml, add user table

---
+++ src/main/java/ku/cs/restaurant/entities/User.java
@@ -0,0 +1,72 @@
+package ku.cs.restaurant.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "User info")
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    private String role;
+    private String name;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
