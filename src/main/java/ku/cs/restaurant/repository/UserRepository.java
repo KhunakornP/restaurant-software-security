22ff294488a6bf3b692fff2e5dec74b8c84f1d95
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-10-17 11:12:26 +0700

    create user repository

---
+++ src/main/java/ku/cs/restaurant/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package ku.cs.restaurant.repository;
+
+
+import ku.cs.restaurant.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.UUID;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByUsername(String username);
+    boolean existsByUsername(String username);
+}
