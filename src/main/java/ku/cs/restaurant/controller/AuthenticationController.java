13cc6854258c3731f6b85014ecee32e8a23cc5cd
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-10-24 10:51:26 +0700

    implement jwt authentication

---
+++ src/main/java/ku/cs/restaurant/controller/AuthenticationController.java
@@ -0,0 +1,64 @@
+package ku.cs.restaurant.controller;
+
+import ku.cs.restaurant.dto.LoginRequest;
+import ku.cs.restaurant.dto.SignupRequest;
+import ku.cs.restaurant.security.JwtUtil;
+import ku.cs.restaurant.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+
+    private UserService userService;
+    private AuthenticationManager authenticationManager;
+    private JwtUtil jwtUtils;
+
+
+    @Autowired
+    public AuthenticationController(UserService userService,
+                                    AuthenticationManager authenticationManager, JwtUtil jwtUtils) {
+        this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @PostMapping("/login")
+    public String authenticateUser(@RequestBody LoginRequest request) {
+
+        Authentication authentication =
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                request.getUsername(),
+                                request.getPassword()
+                        )
+                );
+        UserDetails userDetails =
+                (UserDetails) authentication.getPrincipal();
+        return jwtUtils.generateToken(userDetails.getUsername());
+    }
+
+
+    @PostMapping("/signup")
+    public String registerUser(@RequestBody SignupRequest request) {
+
+
+        if (userService.userExists(request.getUsername()))
+            return "Error: Username is already taken!";
+
+
+        userService.createUser(request);
+        return "User registered successfully!";
+    }
+}
+
+
--- src/main/java/ku/cs/restaurant/security/SecurityConfiguration.java
+++ src/main/java/ku/cs/restaurant/security/SecurityConfiguration.java
@@ -1,18 +1,44 @@
 package ku.cs.restaurant.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 public class SecurityConfiguration {
+
+    private UnauthorizedEntryPointJwt unauthorizedHandler;
+
+    @Autowired
+    public SecurityConfiguration(UnauthorizedEntryPointJwt unauthorizedHandler) {
+        this.unauthorizedHandler = unauthorizedHandler;
+    }
+
+
+    @Bean
+    public JwtAuthFilter authenticationJwtTokenFilter() {
+        return new JwtAuthFilter();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
 
     @Bean
@@ -22,8 +48,15 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
 
 
+
+
                 // Set session management to stateless
                 .sessionManagement(sessionMnt ->                   sessionMnt.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+
+                // Set unauthorized requests exception handler
+                .exceptionHandling(exceptionHandling ->             exceptionHandling.authenticationEntryPoint(unauthorizedHandler)
                 )
 
 
@@ -37,18 +70,18 @@ public class SecurityConfiguration {
                                 // All other endpoints require authentication
                                 .anyRequest().authenticated()
                 );
+
+
+        // Add the JWT Token filter
+        http.addFilterBefore(authenticationJwtTokenFilter(),
+                UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // You can customize parameters for memory, iterations, parallelism
-        // based on your security requirements and system resources.
-        // Recommended values are at least 19 MiB memory, 2 iterations,
-        // and 1 parallelism.
-        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
-        // Or specify custom parameters:
-        // return new Argon2PasswordEncoder(16, 32, 1, 19 * 1024, 2);
+        return new BCryptPasswordEncoder();
     }
 
 
