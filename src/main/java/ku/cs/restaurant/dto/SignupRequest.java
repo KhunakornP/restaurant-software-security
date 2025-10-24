dda11bd4325e3389d5eb2ff835fc6436ff966fdb
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-10-24 10:02:04 +0700

    add basic authentication

---
+++ src/main/java/ku/cs/restaurant/dto/SignupRequest.java
@@ -0,0 +1,34 @@
+package ku.cs.restaurant.dto;
+
+
+public class SignupRequest {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
+
