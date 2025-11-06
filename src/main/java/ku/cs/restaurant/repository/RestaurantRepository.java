015c7b766d65c05554a6af43f8a166dbbe877545
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-11-06 20:47:35 +0700

    update restaurant table

--- src/main/java/ku/cs/restaurant/repository/RestaurantRepository.java
+++ src/main/java/ku/cs/restaurant/repository/RestaurantRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
+import java.util.Optional;
 import java.util.UUID;
 import java.util.List;
 
@@ -13,7 +14,8 @@ import java.util.List;
 public interface RestaurantRepository
         extends JpaRepository<Restaurant, UUID> {
 
-    Restaurant findByName(String name);
+    boolean existsByName(String name);
+    Optional<Restaurant> findByName(String name);
     List<Restaurant> findByLocation(String location);
 
 }
