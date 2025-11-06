d572760e0cdac7ca184beee9ac62548f6a34fca6
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-11-06 22:06:47 +0700

    add exceptions from JwtUtil, JwtAuthFilter to GlobalExceptionHandler

--- src/main/java/ku/cs/restaurant/controller/GlobalExceptionHandler.java
+++ src/main/java/ku/cs/restaurant/controller/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -19,6 +21,7 @@ import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -29,48 +32,56 @@ public class GlobalExceptionHandler {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(error ->
                 errors.put(error.getField(), error.getDefaultMessage()));
+        logger.error("JSON Input Error: {}", errors);
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleEntityNotFoundExceptions(
             EntityNotFoundException ex) {
+        logger.error("Entity not found: {}", ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(EntityExistsException.class)
     public ResponseEntity<String> handleEntityExistsExceptions(
             EntityExistsException ex) {
+        logger.error("Entity already exists: {}", ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(SecurityException.class)
     public ResponseEntity<String> handleSecurityExceptions(
             SecurityException ex) {
+        logger.error("Security exception: {}", ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(MalformedJwtException.class)
     public ResponseEntity<String> handleMalformedJwtExceptions(
             MalformedJwtException ex) {
+        logger.error("Malformed JWT: {}", ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(ExpiredJwtException.class)
     public ResponseEntity<String> handleExpiredJwtExceptions(
             ExpiredJwtException ex) {
+        logger.error("JWT expired: {}", ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(UnsupportedJwtException.class)
     public ResponseEntity<String> handleUnsupportedJwtExceptions(
             UnsupportedJwtException ex) {
+        logger.error("JWT Scheme unsupported: {}", ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentExceptions(
             IllegalArgumentException ex) {
+        logger.error("Illegal argument passed: {}", ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
